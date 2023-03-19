9a52ec7af1544cae6ac682755f23ecf23856ee99
Saksham Gambhir <100534890+sakshxm08@users.noreply.github.com>
2023-03-19 19:58:21 +0530

    Customize Theme

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let college = document.getElementById("college");
 let details = document.getElementById("details");
 let orangeButton = document.getElementById("orange");
 let blueButton = document.getElementById("blue");
+let pickTheme = document.getElementById("pickTheme");
 let purpleButton = document.getElementById("purple");
 let lightVioletButton = document.getElementById("lightViolet");
 let currentTheme = document.getElementById("themeClr");
@@ -51,21 +52,45 @@ college.addEventListener("click", () => {
               >
               <span class="result"><span class="exam">1st Year GPA -</span> 9.38</span>`;
 });
-
-blueButton.addEventListener("click", () => {
-  root.style.setProperty("--bg-dark", "#04151f");
-  root.style.setProperty("--bg-light", "#7cb4b8");
-  root.style.setProperty("--text-dark", "#04151f");
-  root.style.setProperty("--text-light", "#d1e8ea");
-  root.style.setProperty("--bg-sidebar", "#003b5d");
-  root.style.setProperty("--bg-sidebar-fade", "#0f3349");
-  root.style.setProperty("--link-selection", "#d5c5c8");
-  root.style.setProperty("--link-hover", "#86626e");
-  root.style.setProperty("--bg-themeBtn", "#d8d8d8");
-  root.style.setProperty("--bg-themeBtn-hover", "#b8b8b8");
+const setTheme = (
+  bgDark,
+  bgLight,
+  textDark,
+  textLight,
+  bgSidebar,
+  bgSidebarFade,
+  linkSelection,
+  linkHover,
+  bgThemeBtn,
+  bgThemeBtnHover
+) => {
+  root.style.setProperty("--bg-dark", bgDark);
+  root.style.setProperty("--bg-light", bgLight);
+  root.style.setProperty("--text-dark", textDark);
+  root.style.setProperty("--text-light", textLight);
+  root.style.setProperty("--bg-sidebar", bgSidebar);
+  root.style.setProperty("--bg-sidebar-fade", bgSidebarFade);
+  root.style.setProperty("--link-selection", linkSelection);
+  root.style.setProperty("--link-hover", linkHover);
+  root.style.setProperty("--bg-themeBtn", bgThemeBtn);
+  root.style.setProperty("--bg-themeBtn-hover", bgThemeBtnHover);
   meta.name = "theme-color";
-  meta.content = "#04151f";
+  meta.content = bgDark;
   document.getElementsByTagName("head")[0].appendChild(meta);
+};
+blueButton.addEventListener("click", () => {
+  setTheme(
+    "#04151f",
+    "#7cb4b8",
+    "#04151f",
+    "#d1e8ea",
+    "#003b5d",
+    "#0f3349",
+    "#d5c5c8",
+    "#86626e",
+    "#d8d8d8",
+    "#b8b8b8"
+  );
 
   currentTheme.src = "./content/blue.png";
   currentTheme.alt = "Blue";
@@ -75,19 +100,18 @@ blueButton.addEventListener("click", () => {
     "url(./content/Portfolio-Mockup-Blue.png)";
 });
 orangeButton.addEventListener("click", () => {
-  root.style.setProperty("--bg-dark", "#090446ff");
-  root.style.setProperty("--bg-light", "#607b7dff");
-  root.style.setProperty("--text-dark", "#090446ff");
-  root.style.setProperty("--text-light", "#fcb07eff");
-  root.style.setProperty("--bg-sidebar", "#051923ff");
-  root.style.setProperty("--bg-sidebar-fade", "rgb(27, 41, 49)");
-  root.style.setProperty("--link-selection", "rgb(133, 124, 226)");
-  root.style.setProperty("--link-hover", "rgb(85, 189, 179)");
-  root.style.setProperty("--bg-themeBtn", "#bebebe");
-  root.style.setProperty("--bg-themeBtn-hover", "#8a8a8a");
-  meta.name = "theme-color";
-  meta.content = "#090446";
-  document.getElementsByTagName("head")[0].appendChild(meta);
+  setTheme(
+    "#090446ff",
+    "#607b7dff",
+    "#090446ff",
+    "#fcb07eff",
+    "#051923ff",
+    "rgb(27, 41, 49)",
+    "rgb(133, 124, 226)",
+    "rgb(85, 189, 179)",
+    "#bebebe",
+    "#8a8a8a"
+  );
   currentTheme.src = "./content/orange.png";
   currentTheme.alt = "Orange";
   projectCardOnetap.style.backgroundImage =
@@ -96,19 +120,18 @@ orangeButton.addEventListener("click", () => {
     "url(./content/Portfolio-Mockup-Orange.png)";
 });
 purpleButton.addEventListener("click", () => {
-  root.style.setProperty("--bg-dark", "#161b33ff");
-  root.style.setProperty("--bg-light", "#a69cacff");
-  root.style.setProperty("--text-dark", "#161b33ff");
-  root.style.setProperty("--text-light", "#f1dac4ff");
-  root.style.setProperty("--bg-sidebar", "#474973ff");
-  root.style.setProperty("--bg-sidebar-fade", "rgb(49, 50, 76)");
-  root.style.setProperty("--link-selection", "#dbcbd8");
-  root.style.setProperty("--link-hover", "#564787");
-  root.style.setProperty("--bg-themeBtn", "#bebebe");
-  root.style.setProperty("--bg-themeBtn-hover", "#8a8a8a");
-  meta.name = "theme-color";
-  meta.content = "#161b33";
-  document.getElementsByTagName("head")[0].appendChild(meta);
+  setTheme(
+    "#161b33ff",
+    "#a69cacff",
+    "#161b33ff",
+    "#f1dac4ff",
+    "#474973ff",
+    "rgb(49, 50, 76)",
+    "#dbcbd8",
+    "#564787",
+    "#bebebe",
+    "#8a8a8a"
+  );
   currentTheme.src = "./content/purple.png";
   currentTheme.alt = "Purple";
   projectCardOnetap.style.backgroundImage =
@@ -117,19 +140,18 @@ purpleButton.addEventListener("click", () => {
     "url(./content/Portfolio-Mockup-Purple.png)";
 });
 lightVioletButton.addEventListener("click", () => {
-  root.style.setProperty("--bg-dark", "rgb(101, 110, 238)");
-  root.style.setProperty("--bg-light", "rgb(216, 225, 255)");
-  root.style.setProperty("--text-dark", "#0c120cff");
-  root.style.setProperty("--text-light", "#f6f8ffff");
-  root.style.setProperty("--bg-sidebar", "rgb(83, 87, 150)");
-  root.style.setProperty("--bg-sidebar-fade", "rgb(53, 57, 120)");
-  root.style.setProperty("--link-selection", "#c0a2ed");
-  root.style.setProperty("--link-hover", "#64539f");
-  root.style.setProperty("--bg-themeBtn", "#cbcbcb");
-  root.style.setProperty("--bg-themeBtn-hover", "#b2b2b2");
-  meta.name = "theme-color";
-  meta.content = "rgb(101, 110, 238)";
-  document.getElementsByTagName("head")[0].appendChild(meta);
+  setTheme(
+    "rgb(101, 110, 238)",
+    "rgb(216, 225, 255)",
+    "#0c120cff",
+    "#f6f8ffff",
+    "rgb(83, 87, 150)",
+    "rgb(53, 57, 120)",
+    "#c0a2ed",
+    "#64539f",
+    "#cbcbcb",
+    "#b2b2b2"
+  );
   currentTheme.src = "./content/light-violet.png";
   currentTheme.alt = "Light Violet";
   projectCardOnetap.style.backgroundImage =
@@ -137,41 +159,94 @@ lightVioletButton.addEventListener("click", () => {
   projectCardPortfolio.style.backgroundImage =
     "url(./content/Portfolio-Mockup-Light-Violet2.png)";
 });
-hamburger.addEventListener("click", () => {
+pickTheme.addEventListener("click", () => {
+  document.getElementById("customize").style.display = "flex";
+});
+const toggleClass = () => {
   sidebar.classList.toggle("hide");
   hamburger.classList.toggle("hide");
   document.getElementById("cross").classList.toggle("hide");
-});
-document.getElementById("cross").addEventListener("click", () => {
-  sidebar.classList.toggle("hide");
-  hamburger.classList.toggle("hide");
-  document.getElementById("cross").classList.toggle("hide");
-});
+};
+const hideFn = (btn) => {
+  document.getElementById(btn).addEventListener("click", () => {
+    toggleClass();
+  });
+};
+hideFn("hamburger");
+hideFn("cross");
+
 if (window.matchMedia("(max-width:1000px)").matches) {
   sidebar.classList.add("hide");
-  document.getElementById("homeBtn").addEventListener("click", () => {
-    sidebar.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    document.getElementById("cross").classList.toggle("hide");
-  });
-  document.getElementById("aboutBtn").addEventListener("click", () => {
-    sidebar.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    document.getElementById("cross").classList.toggle("hide");
-  });
-  document.getElementById("eduBtn").addEventListener("click", () => {
-    sidebar.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    document.getElementById("cross").classList.toggle("hide");
-  });
-  document.getElementById("projectBtn").addEventListener("click", () => {
-    sidebar.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    document.getElementById("cross").classList.toggle("hide");
-  });
-  document.getElementById("contactBtn").addEventListener("click", () => {
-    sidebar.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    document.getElementById("cross").classList.toggle("hide");
+
+  hideFn("homeBtn");
+  hideFn("aboutBtn");
+  hideFn("eduBtn");
+  hideFn("projectBtn");
+  hideFn("contactBtn");
+}
+const screen = window.matchMedia("(max-width: 1000px)");
+
+screen.onchange = (e) => {
+  if (e.matches) {
+    sidebar.classList.add("hide");
+
+    hideFn("homeBtn");
+    hideFn("aboutBtn");
+    hideFn("eduBtn");
+    hideFn("projectBtn");
+    hideFn("contactBtn");
+    if (hamburger.classList.contains("hide")) {
+      hamburger.classList.remove("hide");
+    }
+    if (!document.getElementById("cross").classList.contains("hide")) {
+      document.getElementById("cross").classList.add("hide");
+    }
+  } else {
+    if (sidebar.classList.contains("hide")) {
+      sidebar.classList.remove("hide");
+    }
+    if (!document.getElementById("cross").classList.contains("hide")) {
+      document.getElementById("cross").classList.add("hide");
+    }
+  }
+};
+
+picker = document.querySelector(".picker");
+for (let i = 0; i < picker.children.length; i++) {
+  picker.children[i].children[0].addEventListener("input", () => {
+    document.querySelector(".theme").children[i].style.backgroundColor =
+      picker.children[i].children[0].value;
   });
 }
+const colorCalc = (color, div) => {
+  let color2 = "#";
+  for (let i = 1; i < color.length; i += 2) {
+    if (i + 1 < color.length) {
+      let num = parseInt(color[i], 16) * 10 + parseInt(color[i + 1], 16);
+      let num2 = parseInt(num / div);
+
+      if (num2 < 10) {
+        color2 += "0";
+      }
+      color2 += num2.toString(16);
+    }
+  }
+  return color2;
+};
+let apply = document.getElementById("applyTheme");
+apply.addEventListener("click", () => {
+  setTheme(
+    picker.children[0].children[0].value,
+    picker.children[1].children[0].value,
+    picker.children[2].children[0].value,
+    picker.children[3].children[0].value,
+    colorCalc(picker.children[0].children[0].value, 2),
+    colorCalc(picker.children[0].children[0].value, 3),
+    picker.children[3].children[0].value,
+    colorCalc(picker.children[3].children[0].value, 2),
+    "#d8d8d8",
+    "#b8b8b8"
+  );
+  console.log(colorCalc(picker.children[3].children[0].value, 2));
+  document.getElementById("customize").style.display = "none";
+});
